57f55a2d4271d3435e147a804cf75d94f14d5628
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-26 21:20:22 +0530

    added db connection to mongodb and node

--- max-mongodb-project/backend/app.js
+++ max-mongodb-project/backend/app.js
@@ -1,28 +1,40 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const productRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth');
+const express = require("express");
+const bodyParser = require("body-parser");
+// mongoclient
+const mongodb = require("mongodb").MongoClient;
+const productRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join('backend/images')));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   // Set CORS headers so that the React SPA is able to communicate with this server
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.use('/products', productRoutes);
-app.use('/', authRoutes);
-
+app.use("/products", productRoutes);
+app.use("/", authRoutes);
+// connect to devConnector cluster with shop db
+mongodb
+  .connect(
+    "mongodb+srv://webdev-mongodb:webdev-mongodb@devconnector.ahxye.mongodb.net/shop?retryWrites=true"
+  )
+  .then((client) => {
+    console.log("Connected");
+    client.close();
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
 app.listen(3100);
